4228e6b2e71adbad4c69bb47a81e68931729f367
Frost2007 <vip.psn2007@mail.ru>
2022-02-12 23:36:09 +0400

    Initial commit

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+
+public class Task {
+    private String taskName;
+    private String description;
+    private Date deadline;
+    private TaskStatus status;
+}
