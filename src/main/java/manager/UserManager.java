fd1df5fe87263907e69caeff93c88e29d2105111
Frost2007 <vip.psn2007@mail.ru>
2022-02-13 02:36:24 +0400

    taskManager

--- src/main/java/manager/UserManager.java
+++ src/main/java/manager/UserManager.java
@@ -1,35 +1,69 @@
 package manager;
 
-import DB.DBConnectionProvider;
+import db.DBConnectionProvider;
 import model.User;
+import model.UserType;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserManager {
 
-   private Connection connection = DBConnectionProvider.getInstance().getConnection();
+    private Connection connection = DBConnectionProvider.getInstance().getConnection();
 
-   public boolean register(User user){
-       String sql = "INSERT INTO user(name,surname,email,password,type) VALUES(?,?,?,?,?)";
+    public boolean register(User user) {
+        String sql = "INSERT INTO user(name,surname,email,password,type) VALUES(?,?,?,?,?)";
 
-       try {
-           PreparedStatement statement= connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-           statement.setString(1, user.getName());
-           statement.setString(2, user.getSurname());
-           statement.setString(3, user.getEmail());
-           statement.setString(4, user.getPassword());
-           statement.setString(5,user.getType().toString());
-           statement.executeUpdate();
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getSurname());
+            statement.setString(3, user.getEmail());
+            statement.setString(4, user.getPassword());
+            statement.setString(5, user.getType().toString());
+            statement.executeUpdate();
 
-           ResultSet rs = statement.getGeneratedKeys();
-           if (rs.next()){
-               user.setId(rs.getInt(1));
-               return true;
-           }
-       } catch (SQLException e) {
-           e.printStackTrace();
-       }
-       return false;
-   }
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM USER";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                users.add(getUserFromResultSet(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    private User getUserFromResultSet(ResultSet resultSet) {
+        try {
+            return User.builder()
+                    .id(resultSet.getInt(1))
+                    .name(resultSet.getString(2))
+                    .surname(resultSet.getString(3))
+                    .email(resultSet.getString(4))
+                    .password(resultSet.getString(5))
+                    .type(UserType.valueOf(resultSet.getObject(6).toString()))
+                    .build();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
