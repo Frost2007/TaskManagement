c9b54f735e682e9653743f66656376a91485c5e8
Frost2007 <vip.psn2007@mail.ru>
2022-02-15 23:07:24 +0400

    taskManager

--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +23,7 @@ public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         for (Task task : tasks) {
             if (req.getAttribute("email").equals(task.getUser().getEmail())){
-                taskManager.getTaskByUserEmail(user);
+                taskManager.getTaskByUserId(user);
 
             }resp.sendRedirect("user.jsp");
         }
