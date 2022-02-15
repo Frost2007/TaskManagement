51b3b47891758344719c543c10c39f0059898f6a
Frost2007 <vip.psn2007@mail.ru>
2022-02-15 23:05:51 +0400

    taskManager

--- src/main/java/servlet/LogInServlet.java
+++ src/main/java/servlet/LogInServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
+import manager.TaskManager;
 import manager.UserManager;
+import model.Task;
 import model.User;
 import model.UserType;
 
@@ -10,10 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/login")
 public class LogInServlet extends HttpServlet {
     private final UserManager userManager = new UserManager();
+    private final TaskManager taskManager = new TaskManager();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,8 +32,11 @@ public class LogInServlet extends HttpServlet {
                 req.getRequestDispatcher("manager.jsp").forward(req, resp);
             }
             if (user.getType().equals(UserType.USER)) {
+                List<Task> tasks = taskManager.getTaskByUserId(user);
+                req.setAttribute("tasks",tasks);
                 req.setAttribute("name", user.getName());
                 req.setAttribute("surname", user.getSurname());
+
                 req.getRequestDispatcher("user.jsp").forward(req, resp);
             }
         }
